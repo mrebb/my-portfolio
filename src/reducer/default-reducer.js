1591c53fda107584e1e81b156ea66a3a1a6bb900
‘MR’ <madhu.rebbana@gmail.com>
2018-11-02 06:47:42 -0700

    about me added

--- src/reducer/default-reducer.js
+++ src/reducer/default-reducer.js
@@ -3,7 +3,7 @@ let initialState = [];
 export default (state = initialState, action) => {
 
   let {type, payload} = action;
-
+  console.log(payload)
   switch(type) {
     default: return state;
   }
